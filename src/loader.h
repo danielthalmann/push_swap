a53dd6acf227495e4ee07a5d2e2349688c003a8c
Daniel Thalmann <daniel@thalmann.li>
2024-12-20 14:18:25 +0100

    loader

--- src/loader.h
+++ src/loader.h
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   loader.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dthalman <daniel@thalmann.li>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -10,32 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "list/list.h"
-#include "io/print.h"
-#include "exceptions/exceptions.h"
-#include "ps/push_swap.h"
-#include "string/string.h"
-#include "loader.h"
+#ifndef LOADER_H
+# define LOADER_H
 
-int	main(int argc, char **argv)
-{
-	int		ret;
-	t_stack	*stack;
+int	load(int argc, char **argv, t_stack **stack);
 
-	ret = load(argc, argv, &stack);
-	if (ret)
-		return (ret);
-	ps_print_stack(stack);
-	ps_ra(stack);
-	ps_ra(stack);
-	ps_sa(stack);
-	ps_rra(stack);
-	ps_sa(stack);
-	ps_pa(stack);
-	ps_print_stack(stack);
-	ps_pb(stack);
-	ps_print_stack(stack);
-	ps_destroy_stack(stack);
-	print("ok\n");
-	return (0);
-}
+#endif
